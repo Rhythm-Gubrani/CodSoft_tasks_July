c52467975659a40b161b66acf1334b0ab873e050
Rhythm-Gubrani <131393410+Rhythm-Gubrani@users.noreply.github.com>
2023-08-18 16:39:01 +0530

    Update Task2_Calculator.cpp

--- Task2_Calculator.cpp
+++ Task2_Calculator.cpp
@@ -19,19 +19,16 @@ int main()
     string choice;
 
     cout<<"-------------------WELCOME-----------------------------\n";
-    cout << "Enter your choice(Y/N)::";
-    cin >> choice;
 
-    while (choice == "Y" || choice=="y")
-    {
+    do{
         cout << "Enter the first number::";
         cin >> num1;
         cout << "Enter the second number::";
         cin >> num2;
-        cout << "\n1.To perform Addition operation\n";
-        cout << "2.To perform Subtraction operation\n";
-        cout << "3.To perform Multiplication operation\n";
-        cout << "4.To perform Division operation\n";
+        cout << "\n1.Addition\n";
+        cout << "2.Subtraction\n";
+        cout << "3.Multiplication\n";
+        cout << "4.Division\n";
         cout << "5.Exit\n";
         cout << "\nEnter the option you want to choose::";
         cin >> operation;
@@ -39,12 +36,10 @@ int main()
         switch (operation)
         {
         case 1:
-            cout << "\nYou have choosen to perform addition operation on two numbers!! \n";
             cout << "Sum of two numbers::" << num1 + num2;
             break;
 
         case 2:
-            cout << "You have choosen to perform subtraction operation on two numbers!! \n";
             if (num1 > num2)
             {
                 cout << "Difference of two numbers::" << num1 - num2;
@@ -57,12 +52,10 @@ int main()
             break;
 
         case 3:
-            cout << "You have choosen to perform multiplication operation on two numbers!! \n";
-            cout << "Product of two numbers::" << num1 * num2;
+             cout << "Product of two numbers::" << num1 * num2;
             break;
 
         case 4:
-            cout << "You have choosen to perform division operation on two numbers!! \n";
             if (num2 == 0)
             {
                 cout << "Division by zero can not be performed \n";
@@ -84,6 +77,6 @@ int main()
         cout << "\n\nDo you want to continue??\n";
         cout << "Enter your choice(Y/N)::";
         cin >> choice;
-    }
+    }while (choice == "Y" || choice=="y");
     return 0;
 }
